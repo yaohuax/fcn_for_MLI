34c59076c6c4305acab3e4aa516e7f6bfe7315a2
LH <jin_linhao@hotmail.com>
2017-12-01 18:55:24 -0500

    before testing

--- fcn/dataset/mli.py
+++ fcn/dataset/mli.py
@@ -8,38 +8,94 @@ Created on Wed Nov 29 14:35:09 2017
 from torch.utils import data
 import collections
 import os.path as osp
+import argparse
+import torchvision
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader,Dataset
+import matplotlib.pyplot as plt
+import torchvision.utils
+import numpy as np
+import random
+from PIL import Image
+import torch
+from torch.autograd import Variable
+import PIL.ImageOps    
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import torch.utils.data as data
+import os
+import scipy
+from scipy import ndimage
+import scipy.misc
 
-class segBase(data.Dataset):
-    #mean_bgr = p.array()
-    def __init__(self, root, split = 'train', transform=False):
-        self.root = root
-        self.split = split
-        self._transform = transform
-        
-        dataset_dir = osp.join(self.root, 'MLI')
-        self.file = collections.defaultdict(list)
-        
-#        for split in ['train', 'val']:
-    
-    def __len__(self):
-        return len(self.files[self.split])
+def default_list_reader(fileList):
+    imgList = []
+    with open(fileList, 'r') as file:
+        for line in file.readlines():
+            imgshortList = []
+            imgPath1, imgPath2= line.strip().split(' ')
+            
+            imgshortList.append(imgPath1)
+            imgshortList.append(imgPath2)
+            imgList.append(imgshortList)
+    return imgList
+
+def img_loader(path):
+    img = Image.open(path)
+    return img
+
+
+
+
+
+
+class ImageList(data.Dataset):
+    def __init__(self, fileList, transform=None, list_reader=default_list_reader, img_loader=img_loader):
+        # self.root      = root
+        self.imgList   = list_reader(fileList)
+        # self._transform = transform
+        self.img_loader = img_loader
+        self.mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
 
     def __getitem__(self, index):
-        data_file = self.files[self.split][index]
-        # load image
-        img_file = data_file['img']
-        img = PIL.Image.open(img_file)
-        img = np.array(img, dtype=np.uint8)
-        # load label
-        lbl_file = data_file['lbl']
-        lbl = PIL.Image.open(lbl_file)
-        lbl = np.array(lbl, dtype=np.int32)
-        lbl[lbl == 255] = 1
-        if self._transform:
-            return self.transform(img, lbl)
-        else:
-            return img, lbl
-        
-        
-    
-        
+        final = []
+        [imgPath1, imgPath2] = self.imgList[index]
+        # print "hello"
+        img = self.img_loader(os.path.join("/home/lh/cv_ws/src/fcn_for_MLI/fcn/dataset/images", imgPath1))
+        lbl = self.img_loader(os.path.join("/home/lh/cv_ws/src/fcn_for_MLI/fcn/dataset/labels", imgPath2))
+        img, lbl = self.transform(img, lbl)
+        return img, lbl
+
+    def __len__(self):
+        return len(self.imgList)
+
+    def transform(self, img, lbl):
+        img = np.array(img)
+        lbl = np.array(lbl)
+        img = img[:, :, ::-1]  # RGB -> BGR
+        img = img.astype(np.float64)
+        img -= self.mean_bgr
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).float()
+        lbl = torch.from_numpy(lbl).long()
+        return img, lbl
+
+
+
+
+def main():
+    train_dataloader = torch.utils.data.DataLoader(
+                        ImageList(fileList="/home/lh/cv_ws/src/fcn_for_MLI/train.txt", 
+                        transform=transforms.Compose([ 
+                                transforms.ToTensor(),            ])),
+                        shuffle=False,
+                        num_workers=8,
+                        batch_size=1)
+    for i, data in enumerate(train_dataloader,0):
+        img, lbl= data
+        print img, lbl
+
+if __name__ == '__main__':
+    main()
--- train_fcn32s.py
+++ train_fcn32s.py
@@ -3,7 +3,9 @@ import os.path as osp
 import torchvision.models as models
 import torch
 import torch.nn as nn
-import fcn
+import trainer
+# import fcn
+
 
 configurations = {
     # same configuration as original fcn32s
@@ -43,17 +45,20 @@ def get_parameters(model, bias):
             raise ValueError('Unexpected module: %s' % str(m))
 
 def main():
-    model = fcn.models.FCN32s(n_class = 2)
+    file = '/home/lh/cv_ws/src/fcn_for_MLI/fcn32s_from_caffe.pth'
+    model = fcn.models.FCN32s()
+    model.load_state_dict(torch.load(file))
+    num = model.score_fc.out_channels
+    model.score_fc = nn.Conv2d(num, 2, 1)
+    model.upscore = nn.ConvTranspose2d(2,2,64, stride=32,bias=False)
     start_epoch = 0
     start_iteration = 0
-    vgg16 = models.vgg16(pretrained = True)
-    model.copy_params_from_vgg16(vgg16)
     cfg = configurations
     optim = torch.optim.SGD(
         [
             {'params': get_parameters(model, bias=False)},
             {'params': get_parameters(model, bias=True),
-             'lr': cfg['lr'] * 2, 'weight_decay': 0},
+             'lr': cfg[1]['lr'] * 2, 'weight_decay': 0},
         ],
         lr=cfg['lr'],
         momentum=cfg['momentum'],
@@ -61,3 +66,16 @@ def main():
 
 
 
+
+    train_dataloader = torch.utils.data.DataLoader(
+                        ImageList(fileList="/home/lh/cv_ws/src/fcn_for_MLI/train.txt", 
+                        transform=transforms.Compose([ 
+                                transforms.ToTensor(),            ])),
+                        shuffle=False,
+                        num_workers=8,
+                        batch_size=1)
+    for i, data in enumerate(train_dataloader,0):
+        # img, lbl= data
+        trainer = Trainer(cuda=cuda, optimizer=optim, train_loader=train_dataloader, val_loader=train_dataloader,
+                                max_iter = 500)
+        trainer.train()
